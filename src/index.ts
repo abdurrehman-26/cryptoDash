098be22a0b697c4acd27359f8d2a04b4a912cf5e
abdurehman-26 <ar2522006@gmail.com>
2026-04-09 20:20:24 +0500

    add onstart function callback

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
 import { Elysia } from "elysia";
 import fs from 'fs'
 import { cors } from '@elysiajs/cors'
+import { fetchCrypto } from "./utils/fetch-crypto";
 
-const app = new Elysia().use(cors()).get("/crypto", () => {
+const app = new Elysia().onStart(async () => {
+  await fetchCrypto()
+  console.log(
+    `CryptoDash is running at ${app.server?.hostname}:${app.server?.port}`
+  );
+}).use(cors()).get("/crypto", () => {
   const raw = fs.readFileSync("./data/crypto.json", "utf8");
     return JSON.parse(raw);
 }).listen(3000);
-
-console.log(
-  `CryptoDash is running at ${app.server?.hostname}:${app.server?.port}`
-);
