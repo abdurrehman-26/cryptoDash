cd4eb10f6b55f85b4d865a596834f67a6225e64a
abdurehman-26 <ar2522006@gmail.com>
2026-04-09 16:08:13 +0500

    Refactor code structure, add data directory and setup /crypto route

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
 import { Elysia } from "elysia";
+import fs from 'fs'
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia().get("/", () => {
+  const raw = fs.readFileSync("./data/crypto.json", "utf8");
+    return JSON.parse(raw);
+}).listen(3000);
 
 console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+  `CryptoDash is running at ${app.server?.hostname}:${app.server?.port}`
 );
