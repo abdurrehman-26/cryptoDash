0a1a13871bf068d578969cea8af2f2252df8ff2a
abdurehman-26 <ar2522006@gmail.com>
2026-04-10 09:28:27 +0500

    correct the change access and add inference for negativity of change

--- src/scraper/index.ts
+++ src/scraper/index.ts
@@ -28,16 +28,16 @@ export async function scrapeTop100() {
     const results: CoinData[] = [];
 
     $("table tbody tr").each((i, el) => {
-      if (i >= 5) return;
+      if (i >= 100) return;
 
       const rank = $(el).find('td').eq(1).text().trim();
       const name = $(el).find('td').eq(2).text().trim().split('\n')[0];
       const symbol = $(el).find('td').eq(2).text().trim().split('\n')[2].trim();
       const image = $(el).find('td').eq(2).find('img').attr('src');
       const price = Number($(el).find('td').eq(4).text().trim().replace('$', '').replaceAll(',', ''));
-      const price_change_1h = Number($(el).find('td').eq(5).text().trim().replace('%', ''));
-      const price_change_24h = Number($(el).find('td').eq(7).text().trim().replace('%', ''));
-      const price_change_7d = Number($(el).find('td').eq(8).text().trim().replace('%', ''));
+      const price_change_1h = Number($(el).find('td').eq(5).text().trim().replace('%', '')) * ($(el).find('td').eq(5).find('span').hasClass('gecko-down') ? -1 : 1);
+      const price_change_24h = Number($(el).find('td').eq(6).text().trim().replace('%', '')) * ($(el).find('td').eq(6).find('span').hasClass('gecko-down') ? -1 : 1);
+      const price_change_7d = Number($(el).find('td').eq(7).text().trim().replace('%', '')) * ($(el).find('td').eq(7).find('span').hasClass('gecko-down') ? -1 : 1);
       const volume_24h = Number($(el).find('td').eq(9).text().trim().replace('$', '').replaceAll(',', ''));
       const market_cap = Number($(el).find('td').eq(11).text().trim().replace('$', '').replaceAll(',', ''));
 
